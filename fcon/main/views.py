7a8ec6b773a4596bdf781631c4e66e9c6d95e3b3
Bartek <bartek_pstragowski@o2.pl>
2022-07-15 14:32:16 +0200

    create form

--- fcon/main/views.py
+++ fcon/main/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponseRedirect
+from .forms import sheetCreator
 
 # Create your views here.
 
 def home(response):
     return render(response, "main/home.html", {})
+
+def create(response):
+    if response.method == 'POST':
+        print(response.POST)
+        return HttpResponseRedirect('/', {})
+    else:
+        form = sheetCreator()
+        return render(response, "main/create.html", {"form": form})
