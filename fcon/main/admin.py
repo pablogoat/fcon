1c64d19763aa1b98229fcdca96a46b5993ff813a
Bartek <bartek_pstragowski@o2.pl>
2022-07-15 18:20:59 +0200

    database models created

--- fcon/main/admin.py
+++ fcon/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Sheet)
+admin.site.register(Person)
+admin.site.register(Item)
+admin.site.register(Debetor)
--- fcon/main/models.py
+++ fcon/main/models.py
@@ -1,3 +1,34 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+
+class Sheet(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Person(models.Model):
+    sheet = models.ForeignKey(Sheet, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    balance = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    sheet = models.ForeignKey(Sheet, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    value = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+class Debetor(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.person.name
