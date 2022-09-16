0644e10fbbf5adacdb27e521ee22b85b1d2410ee
Bartek <bartek_pstragowski@o2.pl>
2022-09-16 12:23:48 +0200

    directory changes

--- fcon/settings.py
+++ fcon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p80$78d%en-o@f)vr%hb&5!rei)=kq6hj@&bkt@89g^+tv$7-i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','friend-reckoning.herokuapp.com']
 
--- fcon/urls.py
+++ fcon/urls.py
@@ -18,14 +18,14 @@ from django.urls import path, include
 from register import views as register_views
 
 from django.conf import settings
-from django.views.static import serve
-from django.conf.urls import url
+#from django.views.static import serve
+#from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path('register/', register_views.register, name="register"),
     path('', include("django.contrib.auth.urls"),),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
