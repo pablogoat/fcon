217f871bf3bdeee7ec33cd0e758c3d96aa415533
Bartek <bartek_pstragowski@o2.pl>
2022-09-16 12:38:37 +0200

    url-to-re_path

--- fcon/urls.py
+++ fcon/urls.py
@@ -14,18 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from register import views as register_views
 
 from django.conf import settings
 from django.views.static import serve
-from django.conf.urls import url
+#from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path('register/', register_views.register, name="register"),
     path('', include("django.contrib.auth.urls"),),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
